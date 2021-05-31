f455f6cf547031542b888364c0d72aaed35492bd
Sudhanshu2829 <84763877+Sudhanshu2829@users.noreply.github.com>
2021-05-31 22:48:50 +0530

    Delete numeric to text.py

--- numeric to text.py
+++
@@ -1,7 +0,0 @@
-list1=["one","two","three","four","five","six","seven","eight","nine"]
-n=int(input("enter  a number"))
-i=n-1
-if(i<len(list1)):
-    print(list1[i])
-else:
-    print("number doesn't lie between 0 and 10")
